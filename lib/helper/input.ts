8ceae0d3316b4693e0605483ac0c93cd64c816a2
Samuel <12182390+xCykrix@users.noreply.github.com>
2024-01-18 17:35:12 -0500

    feat: input api changes

--- lib/helper/input.ts
+++ lib/helper/input.ts
@@ -1,53 +1,59 @@
-
 export class CTFInputHelper {
   private _structured = false;
+  private _expression = /(.*)/;
   private _content = 'InvalidInput';
-  private _separator: string | null = null;
 
   public structured(): CTFInputHelper {
     this._structured = true;
     return this;
   }
 
-  public with(v: string): CTFInputHelper {
+  public from(v: string): CTFInputHelper {
     if (this._structured) v = `${Deno.cwd()}/input/${v.match(/(D\d+).ts$/)![1]}.input.txt`;
     this._content = Deno.readTextFileSync(v).trim();
     return this;
   }
 
-  public separate(separator: string): CTFInputHelper {
-    this._separator = separator;
+  public expression(v: RegExp): CTFInputHelper {
+    this._expression = v;
     return this;
   }
 
-  public getString<T extends StringInputType>(type: T): OutputTypes[T] {
+  public parse<T extends BuiltInInputRegExpIdentifier>(type: T): OutputTypes[T] {
     return {
-      [StringInputType.STRING]: this._content! as string,
-      [StringInputType.SEPARATED_STRING]: this._content.split(this._separator!)! as string[],
+      [BuiltInInputRegExpIdentifier.STRING]: (this._content.match(/(.*)/) ?? [''])[0] as string,
+      [BuiltInInputRegExpIdentifier.CHARACTERS]: (this._content.match(/(.)/g) ?? []) as string[],
+      [BuiltInInputRegExpIdentifier.LIST]: (
+        this._content.split('\n').map((v) => {
+          return (v.match(/(.*)/) ?? [''])[0];
+        })
+      ) as string[],
+      [BuiltInInputRegExpIdentifier.LIST_CHARACTERS]: (
+        this._content.split('\n').map((v) => {
+          return v.match(/(.)/g) ?? [];
+        })
+      ) as string[][],
+      [BuiltInInputRegExpIdentifier.EXPRESSION]: (
+        this._content.split('\n').map((v) => {
+          return [...(v.match(this._expression) ?? [])].slice(1);
+        })
+      ) as string[][],
     }[type]! as OutputTypes[T];
   }
-
-  public getList<T extends ListInputType>(type: T): OutputTypes[T] {
-    return {
-      [ListInputType.LIST]: this._content.split('\n') as string[],
-      [ListInputType.SEPARATED_LIST]: this._content.split('\n').map((v) => v.split(this._separator!)) as string[][],
-    }[type] as OutputTypes[T];
-  }
 }
 
-export enum StringInputType {
-  STRING = 'STRING',
-  SEPARATED_STRING = 'SEPARATED_STRING',
-}
-
-export enum ListInputType {
-  LIST = 'LIST',
-  SEPARATED_LIST = 'SEPARATED_LIST',
+export enum BuiltInInputRegExpIdentifier {
+  STRING,
+  CHARACTERS,
+  LIST,
+  LIST_CHARACTERS,
+  EXPRESSION,
 }
 
 interface OutputTypes {
-  [StringInputType.STRING]: string;
-  [StringInputType.SEPARATED_STRING]: string[];
-  [ListInputType.LIST]: string[];
-  [ListInputType.SEPARATED_LIST]: string[][];
+  [BuiltInInputRegExpIdentifier.STRING]: string;
+  [BuiltInInputRegExpIdentifier.CHARACTERS]: string[];
+  [BuiltInInputRegExpIdentifier.LIST]: string[];
+  [BuiltInInputRegExpIdentifier.LIST_CHARACTERS]: string[][];
+  [BuiltInInputRegExpIdentifier.EXPRESSION]: string[][];
 }
